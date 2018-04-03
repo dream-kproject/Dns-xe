0088e2478e34a28833195b6ed83fe026b10824d4
chowolms <chowol_ms@naver.com>
2018-04-03 22:35:45 +0900

    A 레코드 command 추가

--- dns/console.php
+++ dns/console.php
@@ -4,9 +4,32 @@ $dbuser = "";
 $dbpassword = "";
 $dbname = "";
 
+$type = $_GET['type'];
+
 /*DB 연결*/
 $sql = new mysql($dbhost, $dbuser, $dbpassword, $dbname);
 
+function A(){
+  $command = $_GET['command'];
+  $host = $_GET['host'];
+  $name = $_GET['name'];
 
+  if($command = "add"){
+     // add command 일시
+    
+  }else if($command = "del"){
+    // delete command 일시
+    
+  }else if($command = "change"){
+    // change command 일시
+    
+  
+  }else{
+   echo "<script>alert('NOT Command!!'); history.href=-1;</script>"; 
+  }
+  
+}
+
+$type
 
 ?>
